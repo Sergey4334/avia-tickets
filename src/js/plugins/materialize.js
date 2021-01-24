390d66809ccd00c715c7b0b47aba7b2ba0b4fe69
Sergey4334 <sergey.melnichuk92@gmail.com>
2021-01-24 11:39:18 +0300

    Added Data picers and new function

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -4,3 +4,25 @@ import 'materialize-css/dist/js/materialize.min.js';
 //* Init select
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
+
+export function getSelectInstance(elem) {
+  return M.FormSelect.getInstance(elem);
+}
+//* Init Autocomplate
+
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete);
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+//* Init datepicker
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  showClearBtn: true,
+});
+
+export function getDatepickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
